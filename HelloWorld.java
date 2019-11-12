701a1cc526524fd7d47b5434b036f5bfabe59456
OceanBiscuit <43608853+OceanBiscuit@users.noreply.github.com>
2019-11-12 14:19:09 +0800

    Update HelloWorld.java
    Java HelloWorld主程序，这里尤其是标题尽量在修改时填写一下，否则会保存默认的英文

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String [] args){
-    System.out.print("这里是新建文件和修改文件的主要位置,删除的文件可以点击commit(次数)查看");
+    System.out.print("这里是新建文件和修改文件的主要位置,删除的文件可以点击commit(次数)查看.");
   }
 }
